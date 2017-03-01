a00c612166ef10ce9c4fc86eafe89b82439a0e1f
vomenthan <thancn12@gmail.com>
2017-03-01 18:05:46 +0700

    no message

---
+++ buffalo/limitedtimeonly.php
@@ -0,0 +1,134 @@
+<?php include("header.php");?>
+  
+
+<div class="lto-wrapper" style="background: url(/PageFiles/6839/EZ1_LTO_Final.jpg) repeat-y center top;">
+  <div class="lto-bg-top">
+    <img class="lto-header-img" src="./images/limitedtimeonly/lto-title.png" alt="They&#39;re Here For A Short Season. Become A Fan Of These Limited Time Items Before They&#39;re Retired." />
+  </div>
+
+  
+
+  <div class="menu-container">
+    <div class="menu">
+
+     
+
+         <div class="menu__group cf">
+          <div class="menu__image">
+            <img src="./images/limitedtimeonly/BostonLagerBurger-SamAdams-US.png" />
+          </div>
+          <div class="menu__info">
+            
+                  <div class="info__first">
+                    <span class="title">Boston Lager<sup>®</sup> Burger</span>
+                    Our mouthwatering burger topped with swiss cheese, beer braised mushrooms, and a drizzle of house-made garlic mustard.
+                  </div>
+                 
+                  <div class="info__second">
+                    <span class="title">Samuel Adams Boston Lager</span>
+                    As the flagship of Samuel Adams, Boston Lager’s smooth, malty sweetness led the craft beer revolution, and is at its best when followed by a juicy burger.
+                  </div>
+              
+          </div>
+         </div>
+
+    
+
+         <div class="menu__group cf">
+          <div class="menu__image">
+            <img src="./images/limitedtimeonly/SourIrishmanCocktail-KickinShrimpTacos-US.png" />
+          </div>
+          <div class="menu__info">
+            
+                  <div class="info__first">
+                    <span class="title">Kickin' Shrimp Tacos</span>
+                    Three mini flour tortillas packed with popcorn shrimp and our creamy coleslaw. All topped with green onions and house-made zing zing sauce for the perfect kick of heat.
+                  </div>
+                 
+                  <div class="info__second">
+                    <span class="title">Sour Irishman</span>
+                    Made with Jameson®, Midori®, and lime sour mix, topped off with Mist TWST, he’s sweeter than you might think.
+                  </div>
+              
+          </div>
+         </div>
+
+    
+
+         <div class="menu__group cf">
+          <div class="menu__image">
+            <img src="./images/limitedtimeonly/MeltdownBurger-US.png" />
+          </div>
+          <div class="menu__info">
+            
+                  <div class="info__first">
+                    <span class="title">The Meltdown Burger</span>
+                    A juicy, all-beef patty topped with swiss cheese <em>and</em> a breaded, gooey white cheddar cheese wheel, sautéed onions, and a drizzle of our house-made cool heat sauce all on caraway seasoned, toasted bread.
+                  </div>
+                 
+          </div>
+         </div>
+
+    
+
+         <div class="menu__group cf">
+          <div class="menu__image">
+            <img src="./images/limitedtimeonly/UltimateCheeseSampler-US.png" />
+          </div>
+          <div class="menu__info">
+            
+                  <div class="info__first">
+                    <span class="title">Ultimate Cheese Sampler</span>
+                    Bite-size grilled cheese sandwiches with a dusting of Desert Heat™, cheddar cheese curds and mozzarella sticks combine for the ultimate cheesy experience. Served with creamy garlic marinara and southwestern ranch for dipping.
+                  </div>
+                 
+          </div>
+         </div>
+
+    
+
+         <div class="menu__group cf">
+          <div class="menu__image">
+            <img src="./images/limitedtimeonly/AlaskanCodClassic-US.png" />
+          </div>
+          <div class="menu__info">
+            
+                  <div class="info__first">
+                    <span class="title">Alaskan Cod Classic</span>
+                    A breaded, wild-caught Alaskan cod filet, with fresh veggies, melted american cheese, and a house-made tartar sauce, on a toasted brioche bun.
+                  </div>
+                 
+          </div>
+         </div>
+
+    
+
+         <div class="menu__group cf">
+          <div class="menu__image">
+            <img src="./images/limitedtimeonly/SweetSaharanSpiceWings.png" />
+          </div>
+          <div class="menu__info">
+            
+                  <div class="info__first">
+                    <span class="title">Sweet Saharan Spice</span>
+                    Using the exotic flavors of African bird’s eye peppers, this tangy sauce delivers a unique and savory BBQ flavor with a spicy punch.
+                  </div>
+                 
+          </div>
+         </div>
+
+    
+
+        </div>
+      </div>
+  <div class="lto-bottom-cta">
+    <a href="http://buffalowildwings.com/en/locations/"><img class="lto-footer-cta" src="./images/limitedtimeonly/lto-bottom-title.png" alt="Try &#39;Em Before They&#39;re Out Of Season. Get To B-Dubs" />
+  </a></div>
+  </div>
+
+
+   
+
+
+<?php include("footer.php");?>
+  
--- buffalo/wing-tuesdays.php
+++ buffalo/wing-tuesdays.php
@@ -1,21 +1,17 @@
-  <?php include("header.php");?>
-  
-  
-
-
-  <section class="row promo-details allow-overflow">
+ <?php include("header.php");?>
+ <section class="row promo-details allow-overflow">
 
     <div class="inner-hero">
-      <div class="hero-bg mobile-img" data-desktop-img="/Global/bww-bone-in-wings.jpg" data-mobile-img="/Global/bww-bone-in-wings-mobile.jpg">
-        
+      <div class="hero-bg mobile-img" data-desktop-img="./images/bww-bone-in-wings.jpg" data-mobile-img="./images/bww-bone-in-wings-mobile.jpg">
+        <img src="./images/bww-bone-in-wings.jpg" class="mobile-img" alt="" style="margin-left: -720px; display: block;">
 
-     <img src="/public/images/responsive-shim.png?width=1&amp;height=1" class="mobile-img" alt="" />
+    
      
       </div>
 
       <div class="hero-content">
         <div class="hero-container promo-container">
-          <h1>1/2 Price Wing Tuesdays<strong><sup>®</sup></strong></h1>
+          <h1>Wing Tuesdays<strong><sup>®</sup></strong></h1>
           <div><p>The second day of the week is #1 in our hearts. Our award winning Traditional&nbsp;<span class="il">wings</span>&nbsp;are ½-&nbsp;priced* all day, every&nbsp;<span class="aBn"><span class="aQJ">Tuesday</span></span>. And because they are discounted, it’s the perfect time to try a sauce or seasoning you’ve always been curious about. Caribbean Jerk, Teriyaki, Salt &amp; Vinegar—here’s a perfect time to try something new.</p>
 <p><em>*</em><em><span lang="EN">Valid for a limited time, only on Wing Tuesdays®. While supplies last. Prices and participation vary. Valid only on Snack, Small, Medium and Large size Traditional wing orders. Includes side of celery or carrots and ranch or blue cheese. Excludes tax and gratuity. Half-price option not applicable to orders placed for delivery, where available. Not valid with any other discount or offer. See participating location for details. Void where prohibited.&nbsp;</span></em></p></div>
 
@@ -32,8 +28,8 @@
           
 
       <div class="facebook-wrapper">
-        <div id="facebookDetail" class="facebook-detail" data-ga='{&quot;category&quot;:&quot;/en/promos/wing-tuesdays1/&quot;,&quot;action&quot;:&quot;Main_Content&quot;,&quot;opt_label&quot;:&quot;Facebook_Like&quot;}' data-ga-noclick="">
-          <div class="fb-like" data-href="http://www.buffalowildwings.com/en/promos/wing-tuesdays1/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+        <div id="facebookDetail" class="facebook-detail" data-ga='{&quot;category&quot;:&quot;/en/promos/wing-tuesdays/&quot;,&quot;action&quot;:&quot;Main_Content&quot;,&quot;opt_label&quot;:&quot;Facebook_Like&quot;}' data-ga-noclick="">
+          <div class="fb-like" data-href="http://www.buffalowildwings.com/en/promos/wing-tuesdays/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
         </div>
       </div>
 
@@ -42,8 +38,4 @@
 
     </div>
   </section>
-
-
-
-  
-  <?php include("footer.php");?>
+ <?php include("footer.php");?>
